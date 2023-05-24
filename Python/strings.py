98d21026ac1a76d3b1c5f2b4917d39173f4862d2
civil-Ken <tochukwukeze@gmail.com>
2023-05-25 00:32:04 +0100

    changes

---
+++ Python/strings.py
@@ -0,0 +1,27 @@
+#course = 'Python Course for Beginners'
+#print(course)
+#course = "Python's Course for Beginners"
+#print (course)
+
+#course = ''' 
+#Hi Professor Jack Montgomery,
+
+#Thank you for your kind remarks. I will work on
+
+#the soil tests tomorrow and send you the report.
+
+#Regards,
+#Kenneth
+
+#'''
+
+#print (course)
+
+course = 'Python for Beginners'
+print (course[3])
+
+course = 'Python for Beginners'
+print (course[8:12])
+
+name = 'Jennifer'
+print(name[1:-1])
