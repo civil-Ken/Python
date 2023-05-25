e34ecb561c0050a7c005423f011c5c0ad224b8ba
civil-Ken <tochukwukeze@gmail.com>
2023-05-26 00:05:02 +0100

    change

---
+++ Python/string_methods.py
@@ -0,0 +1,9 @@
+course = 'Python for Beginners'
+# print (len(course))
+# print (course.upper())
+#print (course.find('t'))
+#print (course.find ('P'))
+
+print (course.find ('Beginners'))
+print ( course.replace('Beginners','Novice'))
+print ("for" in course )
