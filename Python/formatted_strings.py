cf40a9b852c699ff6cbf0c4e5848e15bed08e481
civil-Ken <tochukwukeze@gmail.com>
2023-05-25 12:21:39 +0100

    changes

---
+++ Python/formatted_strings.py
@@ -0,0 +1,13 @@
+#first_name= 'Kenneth'
+#last_name= ' [Eze]'
+#print (first_name + last_name + " is a coder")
+
+#first_name= 'Kenneth'
+#last_name= 'Eze'
+#message= first_name + ' [' + last_name + '] is a coder'
+#print (message)
+
+first_name= 'Kenneth'
+last_name= 'Eze'
+msg = f'{first_name} [{last_name}] is a coder'
+print (msg)
