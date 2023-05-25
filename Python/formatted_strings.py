c19f85c053cc5af8b4c72b59a0c24d0530b9ed77
civil-Ken <tochukwukeze@gmail.com>
2023-05-25 22:49:44 +0100

    change

--- Python/formatted_strings.py
+++ Python/formatted_strings.py
@@ -7,7 +7,19 @@
 #message= first_name + ' [' + last_name + '] is a coder'
 #print (message)
 
-first_name= 'Kenneth'
-last_name= 'Eze'
-msg = f'{first_name} [{last_name}] is a coder'
-print (msg)
+#first_name= 'Kenneth'
+#last_name= 'Eze'
+#msg = f'{first_name} [{last_name}] is a coder'
+#print (msg)
+
+
+# two different methods
+# one with concatenation and the other with formatted strings
+
+
+first = 'Tochukwu'
+last = 'Eze'
+merge = first + ' [' + last + '] is a coder'
+mg = f'{first} [{last}] is a coder'
+print (merge)
+print (mg)
