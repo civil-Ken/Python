4bc0ee54b63c928c137f0c0fb17dd229c6e2cef8
civil-Ken <tochukwukeze@gmail.com>
2023-05-26 15:51:46 +0100

    change

---
+++ Python/arithmetic_operator.py
@@ -0,0 +1,9 @@
+print (3/8) #returns with a float
+print (3//8) #returns with an integer
+print (3+8)  # Arithmetic operator
+print (3-8)  # subtractio operator
+print (3*8)  # multiplication operator
+print (3 % 8)  #  modulus operator which returns the remainder of the division
+print (3 ** 8)  # exponent operator or [power]
+
+ 
