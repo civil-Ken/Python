e7e0837f24a82a6125103ce139199e1f6953f84f
civil-Ken <tochukwukeze@gmail.com>
2023-05-29 16:40:59 +0100

    change

---
+++ if_statements2.py
@@ -0,0 +1,13 @@
+print ('Please input Hot or Cold below')
+
+Weather= input ('How is the weather today? ')
+
+
+if Weather == 'Hot':
+    print ("It's a hot day")
+    print ("Drink lots of water and go outside")
+
+else:
+    print ("It's a cold day")
+    print ("wear warm clothes")
+print ('Have  a nice day')
