6bc48a28d288ba356cfacf5ce71729726dafb6a0
civil-Ken <tochukwukeze@gmail.com>
2023-05-29 15:39:38 +0100

    change

--- math_function.py
+++ math_function.py
@@ -3,9 +3,14 @@ y = 5.98
 
 #x= 4.8
 #print (abs (-4.8))
+#print (round (y))
+#print (round (-6.5))
 
+x = (600)
 import math
 
 print (math.ceil(y))
-print (math.ceil(5.98))
-print (round (y))
+print (math.ceil(5.5 ))
+print (math.floor(2.9))
+
+print (math.comb(x, 4))
